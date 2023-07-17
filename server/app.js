a15a37a84016b4ac18c25b30d25464cfc77b418a
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-07-17 12:15:41 +0530

    db config

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
+import morgan from 'morgan';
+import {connect} from './config/db_con.js'
 const app = express();
 
 
 //middlewares
+app.use(express.json());;//Send respones in json fomrat
+app.use(morgan);//log requests
 
 //routes
 
 //server config
 const PORT = process.env.PORT || 9000;
-app.listen(PORT,()=>{
-    console.log(`Sever is running on port ${PORT}`);
+app.listen(PORT,async()=>{
+    try {
+        await connect();
+        console.log(`Sever is running on port ${PORT}`);
+    } catch (error) {
+        console.log(err)
+    }
+       
 })
