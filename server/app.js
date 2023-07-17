18901f038714f4eda0406bce6e09b75c4812d0b8
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-07-17 15:03:45 +0530

    server authentication

--- server/app.js
+++ server/app.js
@@ -4,15 +4,20 @@ import morgan from "morgan";
 import { connect } from "./config/db_con.js";
 import router from "./routes/auth.js";
 import dotenv from "dotenv";
+import auth from "./middlewares/auth.js";
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 //middlewares
 app.use(express.json()); //Send respones in json fomrat
-app.use(morgan('tiny')); //log requests
-app.use("/api", router);
+app.use(morgan("tiny")); //log requests
+
 //routes
+app.get("/protected", auth, (req, res) => {
+  return res.status(200).json({ ...req.user._doc });
+});
+app.use("/api", router);
 
 //server config
 const PORT = process.env.PORT || 9000;
