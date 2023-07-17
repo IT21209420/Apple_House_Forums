622df2a8d35c940042606091c9e2e2961890270d
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-07-17 13:51:03 +0530

    Register and login server

--- server/app.js
+++ server/app.js
@@ -1,24 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import morgan from 'morgan';
-import {connect} from './config/db_con.js'
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import { connect } from "./config/db_con.js";
+import router from "./routes/auth.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+
 const app = express();
 
-
 //middlewares
-app.use(express.json());;//Send respones in json fomrat
-app.use(morgan);//log requests
-
+app.use(express.json()); //Send respones in json fomrat
+app.use(morgan('tiny')); //log requests
+app.use("/api", router);
 //routes
 
 //server config
 const PORT = process.env.PORT || 9000;
-app.listen(PORT,async()=>{
-    try {
-        await connect();
-        console.log(`Sever is running on port ${PORT}`);
-    } catch (error) {
-        console.log(err)
-    }
-       
-})
+app.listen(PORT, async () => {
+  try {
+    await connect();
+    console.log(`Sever is running on port ${PORT}`);
+  } catch (error) {
+    console.log(err);
+  }
+});
