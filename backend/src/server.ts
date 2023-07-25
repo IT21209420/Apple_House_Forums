4e5cad4e0e2fae0dfcd4ef2117b4b11feea0e757
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-07-26 02:08:21 +0530

    back end deploy to aws

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,6 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import env from "./util/envValidate";
 import app from "./app";
-// import authRouter from "./routes/auth.js";
-// import postRouter from "./routes/post.js";
-// import auth from "./middlewares/auth.js";
 
 //server config
 const PORT = env.PORT || 9000;
